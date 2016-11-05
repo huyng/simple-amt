4f313860feeb99451107362817e2c9ad16817609
huy <huy@huyng.com>
2016-11-05 10:46:51 -0700

    cleanup

--- show_account_balance.py
+++ show_account_balance.py
@@ -2,8 +2,8 @@ import argparse
 import simpleamt
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(parents=[simpleamt.get_parent_parser()],
-              description="Show your AMT account balance.")
-  args = parser.parse_args()
-  mtc = simpleamt.get_mturk_connection_from_args(args)
-  print mtc.get_account_balance()[0]
+    parser = argparse.ArgumentParser(parents=[simpleamt.get_parent_parser()],
+                                     description="Show your AMT account balance.")
+    args = parser.parse_args()
+    mtc = simpleamt.get_mturk_connection_from_args(args)
+    print mtc.get_account_balance()[0]
